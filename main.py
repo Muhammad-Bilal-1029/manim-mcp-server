d0b3d581a6be8dfc705062fc440b473db6d33f46
Muhammad-Bilal-1029 <bilalahmad10281029@gmail.com>
2026-08-09 02:12:55 -0700

    Add gitignore

--- main.py
+++ main.py
@@ -22,5 +22,5 @@ def roll_dice(n_dice : int = 1)->list[int]:
     return [random.randint(1,6) for _ in range(n_dice)]
 
 if __name__ == "__main__":
-    mcp.run(transport="http", host="0.0.0.0", =8000) 
+    mcp.run(transport="http", host="0.0.0.0", port=8000) 
 
